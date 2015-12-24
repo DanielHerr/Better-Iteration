46fd51eb08882e33f5d77c1bef4df787e7531a70
Daniel Herr <DanielHerr@users.noreply.github.com>
2015-12-23 21:53:33 -0500

    Update and rename objectloop.js to objectloops.js

---
+++ objectloops.js
@@ -0,0 +1,20 @@
+var objectlooptype = "both"
+
+Object.prototype[Symbol.iterator] = function() {
+ const object = this
+ const keys = Object.keys(object)
+ let item = 0
+ return({
+  next: function() {
+   if(item < keys.length) {
+    let result = { value: { key: keys[item], value: object[keys[item]] }, done: false }
+    if(objectlooptype == "key") {
+     result.value = result.value.key
+    } else if(objectlooptype == "value") {
+     result.value = result.value.value
+    }
+    item = item + 1
+    return(result)
+   } else {
+    return({ value: undefined, done: true })
+} } }) }
