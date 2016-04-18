58b90713138004c3a020410a0668b087a6e90861
Daniel Herr <DanielHerr@users.noreply.github.com>
2016-04-17 23:40:51 -0400

    Update loop.js

--- loop.js
+++ loop.js
@@ -1,14 +1,14 @@
 "use strict"
 
 function*loop(item) {
- let itemtype = datatype(item)
- if(itemtype == "object" || itemtype == "function") {
-  for(let key of Object.keys(item)) {
-   yield([ key, item[key], item ])
- } }
- else if(itemtype == "array") {
+ let itemtype = typeof(item)
+ if(itemtype == "object" && Array.isArray(item)) {
   for(let index of Object.keys(item)) {
    yield([ item[index], Number(index), item ])
+ } }
+ else if(itemtype == "object" || itemtype == "function") {
+  for(let key of Object.keys(item)) {
+   yield([ key, item[key], item ])
  } }
  else if(itemtype == "number") {
   for(let number = 1; number <= item; number = number + 1) {
